2065b649e86b201bba5a86fe5d223b67ba058c49
RandyCheBro <kolyavolya@yandex.ru>
2023-12-15 18:05:39 +0300

    creating project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+
+/* import {NavLink,Link} from "react-router-dom"; */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" alt="логотип" />
+          <div>
+            <h3 className="text-uppercase">REACT SNEAKERS</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30 d-flex align-center">
+            <img className="mr-10" width={18} height={18} src="/img/basket.svg" alt="корзина" />
+            <span>1205 руб.</span>
+          </li>
+          <li className="mr-30 d-flex align-center">
+            <img className="mr-10" width={18} height={18} src="/img/like.svg" alt="лайк" />
+            <span>Закладки</span>
+          </li>
+          <li className="d-flex align-center">
+            <img className="mr-10" width={18} height={18} src="/img/profile.svg" alt="иконка-профиля" />
+            <span>Профиль</span>
+          </li>
+        </ul>
       </header>
+      <div className="content p-40">
+        <h1 className="mb-40">Все кроссовки</h1>
+
+        <div className="d-flex">
+          <div className="card">
+            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="плюс" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={133} height={112} src="/img/sneakers/2.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="плюс" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={133} height={112} src="/img/sneakers/3.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="плюс" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img width={133} height={112} src="/img/sneakers/4.jpg" alt="Sneakers" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="плюс" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
